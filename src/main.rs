0fd582197f9afc97fdb81f6fb6d5e531ed1a6a5d
mmdov <mohammadvary@gmail.com>
2025-11-03 14:25:36 +0330

    fully working version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 mod cli;
 mod handlers;
-mod tui;
 mod vault;
 
 // NOTE: take a master password and create a vault with that password
 // that vault is a file inside that file theres our salt, nonce and the ciphered text
 // all the new passwords are being added to that vault
-// TODO: needs some sort of ui
-// TODO: add more todos
+// TODO: rethink the cli to make it more practical
 // FIX: add error handling everyting is panicking right now
 fn main() {
     let commands = cli::parse_cli();
