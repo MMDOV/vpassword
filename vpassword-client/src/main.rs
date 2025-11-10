6e48790359c51bc5663b9e0920ce9b7476200332
mmdov <mohammadvary@gmail.com>
2025-11-10 20:03:44 +0330

    agent and client are connected! we are getting somewhere. ps: this is gonna panic as soon as you run it dont use it yet

--- vpassword-client/src/main.rs
+++ vpassword-client/src/main.rs
@@ -1,12 +1,32 @@
 mod cli;
 mod handlers;
+use std::{io, process::Command};
+use tokio::net::UnixStream;
 
 // NOTE: take a master password and create a vault with that password
 // that vault is a file inside that file theres our salt, nonce and the ciphered text
 // all the new passwords are being added to that vault
 // TODO: rethink the cli to make it more practical
 // FIX: add error handling everyting is panicking right now
-fn main() {
+#[tokio::main]
+async fn main() {
+    Command::new("vpassword-agent").status().unwrap();
+    let stream = UnixStream::connect("/tmp/vault.sock").await.unwrap();
+    loop {
+        stream.writable().await.unwrap();
+
+        match stream.try_write(b"hello world") {
+            Ok(_) => {
+                break;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(_) => {
+                println!("err");
+            }
+        }
+    }
     let commands = cli::parse_cli();
     handlers::handle_command(commands);
 }
